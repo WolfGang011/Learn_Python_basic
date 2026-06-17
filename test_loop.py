5a1adbb00af1ba11e443e7e458db301b48c9df68
WolfGang011 <thaiphong011199@gmail.com>
2026-06-17 22:38:30 +0700

    loop, while

--- test_loop.py
+++ test_loop.py
@@ -1,3 +1,11 @@
-a = range (1, 10, 1)
-for i in a:
-    print(i, end = " ")
+#  Cần chú ý những cái như sau 
+#  range
+#  break, continue 
+# Nested loop : vòng lặp lồng nhau
+
+#  tính Tổng 
+
+n = 10 
+
+for i in range(1, 10, 0):
+    print(i)
---
+++ while.py
@@ -0,0 +1,20 @@
+# n = 1
+# while n <= 5: 
+#     print(n)
+#     n = n + 1
+#     if n == 3:
+#         break
+
+# while True: 
+#     n = int(input("Nhập số: "))
+#     if n > 0:
+#         break
+
+# đếm số lượng chữ số 
+
+n = 1234
+dem = 0 
+while n != 0:
+    dem = dem + 1
+    n = n // 10
+print("Số lượng chữ số là:", dem)
